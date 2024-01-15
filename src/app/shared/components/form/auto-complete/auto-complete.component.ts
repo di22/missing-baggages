4648ecb25e40116126d9b29560bdadb0ee7285e5
di22 <36845367+di22@users.noreply.github.com>
2024-01-15 10:18:27 +0100

    avoid memory leaks

--- src/app/shared/components/form/auto-complete/auto-complete.component.ts
+++ src/app/shared/components/form/auto-complete/auto-complete.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BaseControlComponent } from '../base-control.component';
 import { AsyncPipe } from '@angular/common';
 import { debounceTime, distinctUntilChanged, startWith } from 'rxjs';
 import { MaterialModule } from '../../../external-modules/material-module';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
     selector: 'app-auto-complete',
@@ -12,11 +13,14 @@ import { MaterialModule } from '../../../external-modules/material-module';
     imports: [MaterialModule, ReactiveFormsModule, AsyncPipe]
 })
 export class AutoCompleteComponent extends BaseControlComponent implements OnInit {
+  destroyRef = inject(DestroyRef);
+
   ngOnInit(): void {
     this._control.valueChanges.pipe(
       startWith(''),
      debounceTime(200),
     distinctUntilChanged(),
+    takeUntilDestroyed(this.destroyRef)
     ).subscribe(value => {
         if(value) this.search.emit(value);
       })
