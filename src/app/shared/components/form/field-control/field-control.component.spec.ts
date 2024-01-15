77386b3f372189df3ef2fa6393f726a50f55098f
di22 <36845367+di22@users.noreply.github.com>
2024-01-15 14:54:27 +0100

    cover more test cases

--- src/app/shared/components/form/field-control/field-control.component.spec.ts
+++ src/app/shared/components/form/field-control/field-control.component.spec.ts
@@ -1,23 +1,114 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FieldControlComponent } from './field-control.component';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FieldTypes } from '../form.config';
+
+@Component({
+  selector: 'app-test-coltrol-field',
+  standalone: true,
+  template: `
+   <app-field-control [control]="control" id="test" [label]="lable" [type]="type"></app-field-control>
+
+  `,
+  imports: [FieldControlComponent]
+})
+export class TestFieldControlComponent {
+control: FormControl = new FormControl('');
+lable: string = 'Test';
+type: FieldTypes = 'autocomplete';
+}
+
 
 describe('FieldControlComponent', () => {
-  let component: FieldControlComponent;
-  let fixture: ComponentFixture<FieldControlComponent>;
+  let component: TestFieldControlComponent;
+  let fixture: ComponentFixture<TestFieldControlComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FieldControlComponent]
+      imports: [TestFieldControlComponent, FieldControlComponent, NoopAnimationsModule]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(FieldControlComponent);
+    fixture = TestBed.createComponent(TestFieldControlComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render autocomplete', () => {
+    const fieldComponent = fixture.debugElement.query(By.directive(FieldControlComponent));
+    const autocompleteField = fieldComponent.nativeElement.querySelector('app-auto-complete');
+    const inputField = fieldComponent.nativeElement.querySelector('app-input-control');
+    const numberField = fieldComponent.nativeElement.querySelector('app-number-control');
+    const dateField = fieldComponent.nativeElement.querySelector('app-date-control');
+
+    expect(autocompleteField).toBeTruthy();
+    expect(inputField).toBeFalsy();
+    expect(numberField).toBeFalsy();
+    expect(dateField).toBeFalsy();
+
+    expect(component.control).toBeTruthy();
+    expect(component.lable).toEqual(fieldComponent.componentInstance.label);
+  });
+
+  it('should render input', () => {
+    component.type = 'text';
+    fixture.detectChanges();
+    const fieldComponent = fixture.debugElement.query(By.directive(FieldControlComponent));
+    const autocompleteField = fieldComponent.nativeElement.querySelector('app-auto-complete');
+    const inputField = fieldComponent.nativeElement.querySelector('app-input-control');
+    const numberField = fieldComponent.nativeElement.querySelector('app-number-control');
+    const dateField = fieldComponent.nativeElement.querySelector('app-date-control');
+
+    expect(autocompleteField).toBeFalsy();
+    expect(inputField).toBeTruthy();
+    expect(numberField).toBeFalsy();
+    expect(dateField).toBeFalsy();
+
+    expect(component.control).toBeTruthy();
+    expect(component.lable).toEqual(fieldComponent.componentInstance.label);
+  });
+
+  it('should render number', () => {
+    component.type = 'number';
+    fixture.detectChanges();
+    const fieldComponent = fixture.debugElement.query(By.directive(FieldControlComponent));
+    const autocompleteField = fieldComponent.nativeElement.querySelector('app-auto-complete');
+    const inputField = fieldComponent.nativeElement.querySelector('app-input-control');
+    const numberField = fieldComponent.nativeElement.querySelector('app-number-control');
+    const dateField = fieldComponent.nativeElement.querySelector('app-date-control');
+
+    expect(autocompleteField).toBeFalsy();
+    expect(inputField).toBeFalsy();
+    expect(numberField).toBeTruthy();
+    expect(dateField).toBeFalsy();
+
+    expect(component.control).toBeTruthy();
+    expect(component.lable).toEqual(fieldComponent.componentInstance.label);
+  });
+
+  it('should render date', () => {
+    component.type = 'date';
+    fixture.detectChanges();
+    const fieldComponent = fixture.debugElement.query(By.directive(FieldControlComponent));
+    const autocompleteField = fieldComponent.nativeElement.querySelector('app-auto-complete');
+    const inputField = fieldComponent.nativeElement.querySelector('app-input-control');
+    const numberField = fieldComponent.nativeElement.querySelector('app-number-control');
+    const dateField = fieldComponent.nativeElement.querySelector('app-date-control');
+
+    expect(autocompleteField).toBeFalsy();
+    expect(inputField).toBeFalsy();
+    expect(numberField).toBeFalsy();
+    expect(dateField).toBeTruthy();
+
+    expect(component.control).toBeTruthy();
+    expect(component.lable).toEqual(fieldComponent.componentInstance.label);
   });
 });
--- src/app/shared/components/grid/grid-client-side/grid-client-side.component.spec.ts
+++ src/app/shared/components/grid/grid-client-side/grid-client-side.component.spec.ts
@@ -1,23 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridClientSideComponent } from './grid-client-side.component';
+import { Component } from '@angular/core';
+import { LostBaggage } from 'src/app/domain/lost-baggages/models';
+import { GridColumn } from '../table-config';
+import { By } from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'app-test-grid',
+  standalone: true,
+  template: `
+  <app-grid-client-side class="mt-1" [columns]="columns" [dataSource]="list"></app-grid-client-side>
+  `,
+  imports: [GridClientSideComponent]
+})
+export class TestGridClienSideComponent {
+list: LostBaggage[] = [{date: '01/01/2024', origin: 'Amsterdam airport', destination: 'Cairo airport', baggageCode: 'A1'}];
+columns: GridColumn[] = [
+  {name: 'Baggage Code', property: 'baggageCode'},
+   {name: 'Date', property: 'date'},
+    {name: 'Origin', property: 'origin'},
+     {name: 'Destination', property: 'destination'},
+     {name: 'Item name', property: 'contentName'},
+     {name: 'Price', property: 'price'},
+     {name: 'Quantity', property: 'quantity'}
+    ];
+}
+
 
 describe('GridClientSideComponent', () => {
-  let component: GridClientSideComponent<any[]>;
-  let fixture: ComponentFixture<GridClientSideComponent<any[]>>;
+  let component: TestGridClienSideComponent;
+  let fixture: ComponentFixture<TestGridClienSideComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GridClientSideComponent]
+      imports: [TestGridClienSideComponent, GridClientSideComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(GridClientSideComponent<any[]>);
+    fixture = TestBed.createComponent(TestGridClienSideComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have columns', () => {
+    const gridComponent = fixture.debugElement.query(By.directive(GridClientSideComponent));
+
+    expect(gridComponent.componentInstance.columns).toEqual(component.columns);
+  });
+
+  it('should have list', () => {
+    const gridComponent = fixture.debugElement.query(By.directive(GridClientSideComponent));
+
+    expect(gridComponent.componentInstance.dataSource).toEqual(component.list);
+  });
+
+  it('should extract columns properties', () => {
+    const gridComponent = fixture.debugElement.query(By.directive(GridClientSideComponent));
+
+    expect(gridComponent.componentInstance.columnsProperties?.length).toEqual(component.columns.length);
+  });
+
+  it('should draw one row', () => {
+    const gridComponent = fixture.debugElement.query(By.directive(GridClientSideComponent));
+    fixture.detectChanges();
+    fixture.whenStable().then(_ => {
+      const rows = gridComponent.nativeElement.querySelector('tr[mat-row]');
+      expect(rows?.length).toEqual(component.list.length);
+    })
+
   });
 });
